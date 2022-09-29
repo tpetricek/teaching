b967a93645054783822c466e28b8cf87e31f3872
Tomas Petricek <tomas@tomasp.net>
2022-09-29 12:32:46 +0200

    Add slides script and draft slides

---
+++ 2022/pl-design/slides.fsx
@@ -0,0 +1,240 @@
+#load ".paket/load/Suave.fsx"
+#r "packages/FSharp.Formatting/lib/netstandard2.1/FSharp.Formatting.Common.dll" 
+#r "packages/FSharp.Formatting/lib/netstandard2.1/FSharp.Formatting.Markdown.dll" 
+open System
+open System.IO
+open FSharp.Formatting.Markdown
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let cprint clr fmt = 
+  Printf.kprintf (fun s -> 
+    let old = Console.ForegroundColor
+    Console.ForegroundColor <- clr
+    Console.WriteLine(s)
+    Console.ForegroundColor <- old) fmt
+
+
+let splitAt f l = 
+  let rec loop res acc = function
+    | x::xs when f x -> 
+        if List.isEmpty acc then loop res [x] xs 
+        else loop (List.rev acc::res) [x] xs
+    | x::xs ->
+        loop res (x::acc) xs 
+    | [] -> 
+        let res = if List.isEmpty acc then res else (List.rev acc::res)
+        List.rev res
+  loop [] [] l
+
+//splitAt (fun n -> n%3=0) [0..10]
+
+// --------------------------------------------------------------------------------------
+// Slides parsing and processing
+// --------------------------------------------------------------------------------------
+
+let (|As|) v x = v, x
+
+let parseOptions pars = 
+  pars 
+  |> List.map (function 
+    | [Span([Literal(lit, _)], _)] ->
+        let col = lit.IndexOf(':')
+        lit.Substring(0, col).Trim(), 
+        lit.Substring(col+1).Trim()
+    | li ->
+        failwithf "Option is not a simple literal")
+  |> Map.ofSeq
+
+let extractSlides pars = 
+  let rec loop opts slide group acc pars = 
+    match pars with
+    | HorizontalRule('*', _) :: 
+      ListBlock(_, items, _) :: pars 
+    | HorizontalRule('*', _) :: As [] (items, pars) ->
+        let group = if slide <> [] then (parseOptions opts, List.rev slide)::group else group
+        let acc = if group <> [] then List.rev group::acc else acc
+        loop items [] [] acc pars 
+    | HorizontalRule _ :: 
+      ListBlock(_, items, _) :: pars 
+    | HorizontalRule _ :: As [] (items, pars) ->
+        let group = if slide <> [] then (parseOptions opts, List.rev slide)::group else group
+        loop items [] group acc pars
+    | par :: pars ->
+        loop opts (par::slide) group acc pars 
+    | [] ->
+        let group = if slide <> [] then (parseOptions opts, List.rev slide)::group else group
+        let acc = if group <> [] then List.rev group::acc else acc
+        List.rev acc
+  loop [] [] [] [] pars
+
+// --------------------------------------------------------------------------------------
+// Templates
+// --------------------------------------------------------------------------------------
+
+let (|Headings|_|) = function
+  | (Heading _ as h1) :: (Heading _ as h2) :: body -> Some([h1;h2], body)
+  | (Heading _ as h1) :: body -> Some([h1], body)
+  | _ -> None
+
+let iconsTemplate = function
+  | Headings(hs, [ListBlock(Unordered, items, _)]) ->
+      let items = items |> List.map (function
+        | [Paragraph(Emphasis([Literal(fa, _)], _)::body, _)] 
+        | [Span(Emphasis([Literal(fa, _)], _)::body, _)] -> 
+            let nbody = body |> List.collect (function
+              | HardLineBreak _ -> 
+                  [ HardLineBreak None
+                    Literal("<i class=\"placeholder\"></i>", None) ]
+              | span -> [span])
+            [Span(Literal($"<i class='fa {fa}'></i>", None)::nbody, None)]
+        | item -> failwithf "Icon template item did not start with *fa-xxx*")
+      hs @ [ListBlock(Unordered, items, None)]
+  | _ -> failwith "Unexpected slide structure in icons template"
+
+let imageTemplate = function
+  | Paragraph([DirectImage _ as img], _)::pars ->
+      [ InlineHtmlBlock("<div class=\"body1\">", None, None)
+        Paragraph([img], None)
+        InlineHtmlBlock("</div><div class=\"body2\">", None, None)
+        yield! pars
+        InlineHtmlBlock("</div>", None, None) ]      
+  | _ -> failwith "Image template did not start with an image"
+
+let listsTemplate = function
+  | Headings(hs, Paragraph([DirectImage(_, img, _, _)], _)::pars) ->
+      [ yield! hs
+        yield InlineHtmlBlock($"<img src=\"{img}\"/>", None, None)
+        let groups = pars |> splitAt (function Heading _ -> true | _ -> false)
+        for i, group in Seq.indexed groups do
+          if i <> 0 then yield InlineHtmlBlock("<div class=\"fragment\">", None, None)
+          yield! group
+          if i <> 0 then yield InlineHtmlBlock("</div>", None, None)
+        yield! pars ]
+  | _ -> failwith "Lists template did not start with an image"
+
+let subtitleTemplate pars = 
+  [ InlineHtmlBlock("<div class=\"body\">", None, None)
+    yield! pars
+    InlineHtmlBlock("</div>", None, None) ]
+
+let titleTemplate pars = 
+  let hs, ps = pars |> List.partition (function Heading _ -> true | _ -> false)
+  [ yield! hs 
+    InlineHtmlBlock("<div class=\"body\">", None, None)
+    yield! ps
+    InlineHtmlBlock("</div>", None, None) ]
+
+let templates = 
+  [ "title", titleTemplate >> Some
+    "subtitle", subtitleTemplate >> Some
+    "icons", iconsTemplate >> Some
+    "largeicons", iconsTemplate >> Some
+    "lists", listsTemplate >> Some
+    "image", imageTemplate >> Some
+    "default", Some
+    "notes", fun _ -> None ]
+  |> dict
+
+// --------------------------------------------------------------------------------------
+// File processing 
+// --------------------------------------------------------------------------------------
+
+// let fn, out = "slides/index.md", "output/index.html"
+let processFile fn out = 
+  let doc = Markdown.Parse(File.ReadAllText(fn))
+  let transformSlide (opts, slide) = 
+    templates.[Map.find "template" opts] slide
+
+  let slidesHtml = 
+    extractSlides doc.Paragraphs
+    |> List.map (List.choose (fun (opts, slide) ->
+      match transformSlide (opts, slide) with 
+      | Some nslide -> 
+          let html = Markdown.ToHtml(MarkdownDocument(nslide, doc.DefinedLinks))
+          Some($"""<section class="{opts.["template"]}">{html}</section>""")
+      | _ -> None))
+    |> List.filter (List.isEmpty >> not)
+    |> List.map (fun slides -> $"""<section>{String.concat "" slides}</section>""")
+    |> String.concat ""
+
+  let templ = File.ReadAllText("slides/template.html")
+  let out = templ.Replace("{{BODY}}", slidesHtml)
+  File.WriteAllText("output/index.html", out)
+
+let copyStatic () =
+  let rec loop root = 
+    for dir in Directory.GetDirectories(root) do
+      loop dir
+      Directory.CreateDirectory(dir.Replace("slides","output")) |> ignore
+      for f in Directory.GetFiles(dir) do 
+        cprint ConsoleColor.Gray "[INFO] Copying file: %s" f
+        File.Copy(f, f.Replace("slides", "output"))
+  for dir in Directory.GetDirectories("slides") do
+    Directory.Delete(dir.Replace("slides","output"), true)
+  loop "slides"
+
+let updateSlides () = 
+  for fn in Directory.GetFiles("slides", "*.md") do
+    cprint ConsoleColor.Gray "[INFO] Processing slide: %s" fn
+    let out = $"output/{Path.GetFileNameWithoutExtension(fn)}.html"
+    processFile fn out
+  copyStatic ()
+
+// --------------------------------------------------------------------------------------
+// 
+// --------------------------------------------------------------------------------------
+
+
+let refreshEvent = Event<unit>()
+
+let fsw = new FileSystemWatcher("slides")
+fsw.NotifyFilter <- NotifyFilters.LastWrite
+fsw.Changed.Add(fun e -> 
+  try 
+    cprint ConsoleColor.White "[INFO] Updating slides"
+    updateSlides()
+    refreshEvent.Trigger()
+    cprint ConsoleColor.White "[INFO] Update completed"
+  with e ->
+    cprint ConsoleColor.Red "[ERROR] %s" e.Message    
+)
+fsw.EnableRaisingEvents <- true
+//fsw.Dispose()
+
+
+
+
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
+
+let ws (webSocket : WebSocket) (context: Suave.Http.HttpContext) = socket {
+  while true do
+    do! SocketOp.ofAsync (Async.AwaitEvent refreshEvent.Publish)
+    let msg = System.Text.Encoding.ASCII.GetBytes("refresh!") |> ByteSegment
+    do! webSocket.send Text msg true }
+
+open Suave
+open Suave.Filters
+open Suave.Operators
+
+let app : WebPart =
+  choose [
+    path "/websocket" >=> handShake ws
+    Writers.setHeader "Cache-Control" "no-cache, no-store, must-revalidate"
+    >=> Writers.setHeader "Pragma" "no-cache"
+    >=> Writers.setHeader "Expires" "0"
+    >=> choose [ 
+      path "/" >=> Files.file "./output/index.html"
+      Files.browseHome
+      RequestErrors.NOT_FOUND "Page not found." ]
+  ]
+
+let config =
+  { defaultConfig with 
+      homeFolder = Some (Path.GetFullPath "./output") 
+      compressedFilesFolder = Some(Path.GetTempPath()) }
+
+let _, start = startWebServerAsync config app
+//Async.Start(start)
+Async.RunSynchronously(start)
---
+++ 2022/pl-design/slides/index.md
@@ -0,0 +1,161 @@
+*****************************************************************************************
+- template: title
+
+# NPRG075
+## Programming language design
+
+
+**Tomáš Petříček**, 309 (3rd floor)  
+_<i class="fa fa-envelope"></i>_ [petricek@d3s.mff.cuni.cz](mailto:petricek@d3s.mff.cuni.cz)  
+_<i class="fa-solid fa-circle-right"></i>_ [https://tomasp.net](https://tomasp.net) | [@tomaspetricek](http://twitter.com/tomaspetricek)
+
+**Lectures:** Monday 12:20, S7  
+_<i class="fa-solid fa-circle-right"></i>_ https://d3s.mff.cuni.cz/teaching/nprg075
+
+<img src="img/qr.png" id="qr" />
+
+*****************************************************************************************
+- template: subtitle
+
+# Introduction
+## What? Why? How?
+
+-----------------------------------------------------------------------------------------
+- template: largeicons
+
+# My background
+
+- *fa-landmark* **PhD, University of Cambridge**  
+  Context-aware programming languages
+
+- *fa-industry* **Microsoft Research Cambridge**  
+  F# and applied functional programming
+
+- *fa-city* **The Alan Turing Institute, London**  
+  Expert and non-expert tools for data science
+
+- *fa-church* **University of Kent, Canterbury**  
+  History and programming systems
+
+-----------------------------------------------------------------------------------------
+- template: image
+
+![](img/intro/tapl.png)
+
+# Programming language theory
+
+**Prove properties about small formal models**
+
+"Well-typed programs do not go wrong"
+
+**Avoid subtle mistakes!**
+
+-----------------------------------------------------------------------------------------
+- template: image
+
+![](img/intro/jupyter.png)
+
+# Data science tools and languages
+
+**Result is a document not a program**
+
+**Working with one concrete dataset**
+
+Different language and system requirements!
+
+-----------------------------------------------------------------------------------------
+- template: image
+
+![](img/intro/smalltalk.png)
+
+# Programming systems & history
+
+**Interacting with a stateful environment**
+
+Let programmers do more in new ways...
+
+**It's not just a language!**
+
+-----------------------------------------------------------------------------------------
+- template: lists
+
+# Bring everything together
+
+![](yo.png)
+
+## Systems, not languages
+
+- yo
+- yo
+- yo
+
+## Interdisciplinary research
+
+- yo
+- yo
+- yo
+
+*****************************************************************************************
+- template: icons
+
+# Motivation 1
+## Tools for open data-driven journalism
+
+- *fa-chalkboard-teacher*  Most work done in external libraries!
+- *fa-search* Transparency through openness
+- *fa-database* Uniformity across data sources xx
+- *fa-search* Yet another option
+
+*****************************************************************************************
+- template: notes
+
+Practical
+- About me
+- Idea
+- Credit
+- Plan
+
+Conventionally
+- Functional, OOP, Logic, etc.
+- Features like lambdas, scoping, pointers
+- Parsing and automata
+- Compilers and interpreters
+
+Design
+- Definition
+- Design vs science vs art
+- Making things - bigger picture
+
+Research methods
+- Requirements, Creation, Evaluation
+- Formal
+- Empirical
+- Conceptual
+- History
+- Cognitive
+
+Type Script
+- https://github.com/Microsoft/TypeScript/issues/9825#issuecomment-234115900
+
+Cultures of programming
+- Hackers
+- Engineers
+- Mathematicians
+- Humanists
+- Managers
+
+Interdisciplinarity
+- Combine everything - that is hard but only way!
+
+
+*****************************************************************************************
+- template: icons
+
+# Motivation 4
+## Tools for open data-driven journalism
+
+- *fa-chalkboard-teacher* Simplicity and learnability
+- *fa-search* Transparency through openness
+- *fa-database* Uniformity across data sources xx
+
+*****************************************************************************************
